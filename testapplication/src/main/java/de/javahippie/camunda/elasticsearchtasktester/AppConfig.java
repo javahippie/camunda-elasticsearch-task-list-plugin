0b893ccca5bbc75b20303690781a22aaa5aced6a
Tim Zöller <tim.zoeller@ilume.de>
2019-09-22 16:29:59 +0200

    Restructuring, adding Plugin to the Test Application

---
+++ testapplication/src/main/java/de/javahippie/camunda/elasticsearchtasktester/AppConfig.java
@@ -0,0 +1,18 @@
+package de.javahippie.camunda.elasticsearchtasktester;
+
+import de.javahippie.camunda.ElasticSearchTaskProcessEnginePlugin;
+import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.camunda.bpm.spring.boot.starter.configuration.Ordering;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Order(Ordering.DEFAULT_ORDER + 1)
+    public static ProcessEnginePlugin myCustomConfiguration() {
+        return new ElasticSearchTaskProcessEnginePlugin();
+    }
+}
